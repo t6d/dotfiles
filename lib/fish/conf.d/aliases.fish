56d810e73fc938447a243298fa34faf7ac7ba827
Konstantin Tennhard <konstantin@tennhard.net>
2024-11-18 06:29:15 -0500

    [Fish] Use eza instead of exa

--- lib/fish/conf.d/aliases.fish
+++ lib/fish/conf.d/aliases.fish
@@ -1,2 +1,3 @@
-alias ll "exa -lh --git --time-style=long-iso --group-directories-first"
-alias tree "exa --tree --git-ignore --git --time-style=long-iso --group-directories-first"
+alias ll "eza -lh --git --time-style=long-iso --group-directories-first"
+alias lla "eza -lah --git --time-style=long-iso --group-directories-first"
+alias tree "eza --tree --git-ignore --git --time-style=long-iso --group-directories-first"
