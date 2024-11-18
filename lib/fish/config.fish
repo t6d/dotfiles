ea55ae25f0434c49478d06a98b1bb9d0037b7201
Konstantin Tennhard <konstantin@tennhard.net>
2024-11-18 06:24:15 -0500

    [Fish] Load iTerm specific fish settings

--- lib/fish/config.fish
+++ lib/fish/config.fish
@@ -1,3 +1,4 @@
 if status is-interactive
  alias dokku "ssh dokku@dokku.t6d.dev"
 end
+test -e /Users/t6d/.iterm2_shell_integration.fish ; and source /Users/t6d/.iterm2_shell_integration.fish ; or true
